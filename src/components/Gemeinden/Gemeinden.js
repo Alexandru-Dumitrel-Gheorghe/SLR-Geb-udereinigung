f34cfb5cd556d881a4858bb7d6c22b503f49139b
Alexandru-Dumitrel-Gheorghe <167351297+Alexandru-Dumitrel-Gheorghe@users.noreply.github.com>
2024-11-08 18:24:26 +0100

    Updated Gemeinden

--- src/components/Gemeinden/Gemeinden.js
+++ src/components/Gemeinden/Gemeinden.js
@@ -1,3 +1,4 @@
+// src/components/Gemeinden/Gemeinden.js
 import React from "react";
 import styles from "./Gemeinden.module.css";
 import { motion } from "framer-motion";
@@ -10,7 +11,7 @@ const Gemeinden = () => {
       alt: "Gemeinde Haar",
       name: "Gemeinde Haar",
       description:
-        "Die Gemeinde Haar vertraut auf unsere Dienstleistungen, um öffentliche und private Räume sauber und gepflegt zu halten. Wir gewährleisten eine hygienische und angenehme Umgebung für alle Bewohner.",
+        "Die Gemeinde Haar vertraut auf unsere Dienstleistungen, um öffentliche und private Räume sauber und gepflegt zu halten.",
       link: "#contact",
     },
     {
@@ -18,7 +19,7 @@ const Gemeinden = () => {
       alt: "Gemeinde Oberschleißheim",
       name: "Gemeinde Oberschleißheim",
       description:
-        "Gemeinde Oberschleißheim setzt auf unsere Expertise in der Gebäudereinigung, um höchste Standards in Sauberkeit und Professionalität zu gewährleisten. Unser Service sorgt für eine gepflegte und einladende Umgebung.",
+        "Gemeinde Oberschleißheim setzt auf unsere Expertise in der Gebäudereinigung, um höchste Standards in Sauberkeit zu gewährleisten.",
       link: "#contact",
     },
   ];
@@ -32,9 +33,7 @@ const Gemeinden = () => {
         transition={{ duration: 1 }}
         viewport={{ once: true }}
       >
-        <h2 className={styles.heading}>
-          Gemeinden die auf unsere Dienstleistungen setzen
-        </h2>
+        <h2 className={styles.heading}>Unsere Gemeinden</h2>
         <div className={styles.cardsContainer}>
           {gemeinden.map((gemeinde, index) => (
             <motion.div
